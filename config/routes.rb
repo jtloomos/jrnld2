26fc0845b3e56709699784354584591989c27d12
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-03-04 11:42:58 -0300

    Added tags and reminders create and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   resources :entries
 
+  resources :tags, only: [:create, :update]
+  resources :reminders, only: [:create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
