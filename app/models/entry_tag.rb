e3f6fd0af798d686a8f040d8628544ba7a8a3c7f
Janette Kwan <kkitt@live.com>
2020-03-04 11:40:27 -0300

    all table columns and table connections completed

---
+++ app/models/entry_tag.rb
@@ -0,0 +1,2 @@
+class EntryTag < ApplicationRecord
+end
---
+++ db/migrate/20200304142809_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birthday, :date
+    add_column :users, :gender, :string
+  end
+end
