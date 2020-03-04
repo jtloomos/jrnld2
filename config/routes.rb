edec71b46cd77bbb0374198d1a8db1db64af399d
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-03-04 11:30:08 -0300

    Added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+
+  get 'dashboard', to: 'users#dashboard', as: 'dashboard'
+  get 'preferences', to: 'users#preferences', as: 'preferences'
+  get 'analytics', to: 'users#analytics', as: 'analytics'
+  resources :users, only: [:show, :edit, :update]
+
+  resources :entries
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
