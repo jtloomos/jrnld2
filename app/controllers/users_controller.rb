cd2765692e9cd2960d1598a8e9ac0e72c729dbe6
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-03-05 16:13:40 -0300

    Added logic to the preferences page. Just missing style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,25 +13,24 @@ class UsersController < ApplicationController
   end
 
   def preferences
-    @user_reminders = Reminder.where(user: current_user).map { |r| r.title.upcase}
-    @default_reminders = %w[SPORTS FAMILY FRIENDS SPECIAL ONE]
+    @user_reminders = Reminder.where(user: current_user)
+    @default_reminders = Reminder.where(user: nil)
     @reminder = Reminder.new
   end
 
   def new_preferences
-    user_reminders = Reminder.where(user_id: current_user.id).pluck(:title)
-    reminders = params.select {|key, value| key =~ /reminder-\d/ }
+    user_reminders = Reminder.where(user_id: current_user.id)
+    reminders = params.select {|key, value| key =~ /reminder-\d/ }.values
 
     user_reminders.each do |user_reminder|
-      db_reminder = Reminder.find_by(title: user_reminder)
-      db_reminder.destroy unless reminders.values.include?(user_reminder) || !db_reminder
+      user_reminder.destroy unless reminders.include?(user_reminder.title)
     end
 
-    reminders.each do |key,reminder|
-      Reminder.create(title: reminder, user: current_user) unless user_reminders.include?(reminder)
+    reminders.each do |reminder|
+      Reminder.create(title: reminder, user: current_user) unless user_reminders.pluck(:title).include?(reminder)
     end
 
-    redirect_to
+    redirect_to dashboard_path
   end
 
   def analytics
