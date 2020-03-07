c914d014b91cc286d1c9439e4c9163158c0ecf0f
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-03-07 13:22:49 -0300

    Corrected some style issues. Also added javascript logic to submit a new reminder pressing enter

--- app/javascript/preferences/add.js
+++ app/javascript/preferences/add.js
@@ -1,18 +1,27 @@
 const plus = document.querySelector("#plus-reminder")
 const repo = document.querySelector(".default-reminders")
+const title = document.querySelector("#new-reminder")
 
-const HTML = (title) => {return `<input id="reminder-check-${title}" class ="reminder-check" name="reminder-${title}" type="checkbox" value=${title.toUpperCase()} checked><div class="btn-ghost reminder"><label for="reminder-${title}" class="reminder-label">${title.toUpperCase()}</label></div>`}
+const HTML = (title) => {return `<input id="reminder-check-${title}" class ="reminder-check" name="reminder-${title}" type="checkbox" value=${title.toUpperCase()} checked><div class="btn-ghost reminder"><label for="reminder-check-${title}" class="reminder-label">${title.toUpperCase()}</label></div>`}
+
+const addHTML = (title) => {
+  repo.insertAdjacentHTML("beforeend", HTML(title.value))
+  title.value = ""
+}
 
 if (plus) {
   plus.addEventListener("click", (event) => {
-    const title = document.querySelector("#new-reminder")
     if (title.value !== "") {
-    repo.insertAdjacentHTML("beforeend", HTML(title.value))
+      addHTML(title);
     };
-    title.value = ""
   })
 }
 
-
-
-
+if (title) {
+  title.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addHTML(title);
+    }
+  });
+};
