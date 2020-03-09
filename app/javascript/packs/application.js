59d92dc898086648ca901ec9f53ef66978d5654a
James Loomos <jtloomos@gmail.com>
2020-03-09 12:53:39 -0300

    Completed chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ import { initSelect2 } from '../plugins/init_select2';
 import { initAutocomplete } from "../plugins/init_autocomplete"
 import 'select2/dist/css/select2.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
-require("chartkick")
-require("chart.js")
-
 initMapbox();
 initAutocomplete()
 initSelect2();
