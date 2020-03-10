dbba350e753ede2238eb968185cee38da6d6025d
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-03-10 17:33:05 -0300

    Added destroy entry logic

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -1,6 +1,6 @@
 class Analytic < ApplicationRecord
-  has_many :word_frequencies
-  has_many :name_frequencies
-  has_many :emotions
+  has_many :word_frequencies, dependent: :destroy
+  has_many :name_frequencies, dependent: :destroy
+  has_many :emotions, dependent: :destroy
   belongs_to :entry
 end
