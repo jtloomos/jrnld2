68e7b7285a4b5c3654e3fdabbb62e6523b278d7f
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-03-11 09:43:08 -0300

    Added logic for basic analytics in the user model

--- app/javascript/entries/new.js
+++ app/javascript/entries/new.js
@@ -1,12 +1,18 @@
 document.querySelectorAll('.feedback li').forEach(emoji => emoji.addEventListener('click', e => {
-    if(!emoji.classList.contains('active')) {
-        document.querySelector('.feedback li.active').classList.remove('active');
-        emoji.classList.add('active');
+  if(!emoji.classList.contains('active')) {
+    document.querySelector('.feedback li.active').classList.remove('active');
+    emoji.classList.add('active');
 
-        const newValue = emoji.classList[0];
-        const emojiField = document.querySelector("#emoji-value")
-        emojiField.value = newValue
-    }
-    e.preventDefault();
+    const newValue = emoji.classList[0];
+    const emojiField = document.querySelector("#emoji-value")
+    emojiField.value = newValue
+  }
+  e.preventDefault();
 }));
 
+hidden_field = document.getElementById("entry_start_entry")
+if (hidden_field) {
+  document.addEventListener("keyup", (event) => {
+    hidden_field.value = new Date()
+  });
+};
